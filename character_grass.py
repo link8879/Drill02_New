b20b1a2bb047115a8146bae23ae3a7189c883372
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-21 12:41:42 +0900

    위에서부터 원 그리기 완성 및 그리기 완성

--- character_grass.py
+++ character_grass.py
@@ -16,7 +16,7 @@ def run_circle():
     print('CIRCLE')
 
     cx, cy, r = 400, 300, 200
-    for deg in range(0,360,5):
+    for deg in range(90,450 + 1,5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
         render_frame(x,y)
@@ -27,17 +27,17 @@ def run_rectangle():
     print('RECTANGLE')
 
     # bottom line
-    #for x in range(50,750+1,5):
-        #render_frame(x,90) # x,y위치에 캐릭터 그려줄수 있는 
-
-    # top line
-    #for x in range(750, 50-1, -5):
-        #render_frame(x, 550)
+    for x in range(50,750+1,5):
+        render_frame(x,90) # x,y위치에 캐릭터 그려줄수 있는 
 
     # right line
-    #for y in range(90,550+1,5):
-        #render_frame(750, y)
-    
+    for y in range(90,550+1,5):
+        render_frame(750, y)
+
+    # top line
+    for x in range(750, 50-1, -5):
+        render_frame(x, 550)
+
     # left line
     for y in range(550, 90-1, -5):
         render_frame(50,y)

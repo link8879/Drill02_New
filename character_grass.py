b4e10bf525888fd3c8753474e688310ada60d0c7
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-21 12:01:22 +0900

    원점에서 원 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 
@@ -13,6 +13,16 @@ delay(1)
 
 def run_circle():
     print('CIRCLE')
+
+    r = 200
+    for deg in range(0,360,5):
+        x = r * math.cos(math.radians(deg))
+        y = r * math.sin(math.radians(deg))
+
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
     pass
    
 
@@ -24,7 +34,7 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
-
+    break
 
 
 close_canvas()

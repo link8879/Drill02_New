9fb544686cbefa6e54ffa162fc04967e0a372c46
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-21 11:52:26 +0900

    원, 사각형 그리기 무한반복 프레임 완성

--- character_grass.py
+++ character_grass.py
@@ -11,4 +11,20 @@ grass.draw_now(400,30)
 character.draw_now(400,90)
 delay(1)
 
+def run_circle():
+    print('CIRCLE')
+    pass
+   
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
+while True:
+    run_circle()
+    run_rectangle()
+
+
+
 close_canvas()

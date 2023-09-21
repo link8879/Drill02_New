7f5b04fae4412f41fda68f982eb58bbd663b5dcc
WonHyung Lee(Gram) <link8879@naver.com>
2023-09-21 12:20:28 +0900

    그리기 함수 추가 완료

--- character_grass.py
+++ character_grass.py
@@ -6,10 +6,11 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-clear_canvas_now()
-grass.draw_now(400,30)
-character.draw_now(400,90)
-delay(1)
+def render_frame(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.1)
 
 def run_circle():
     print('CIRCLE')
@@ -18,10 +19,7 @@ def run_circle():
     for deg in range(0,360,5):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
+        render_frame(x,y)
         delay(0.1)
    
 
@@ -30,17 +28,14 @@ def run_rectangle():
 
     # bottom line
     for x in range(50,750+1,5):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
+        render_frame(x,90) # x,y위치에 캐릭터 그려줄수 있는 
     
 
 
 while True:
-    # run_circle()
-    run_rectangle()
-    break
+     run_circle()
+     run_rectangle()
+     break
 
 
 close_canvas()
